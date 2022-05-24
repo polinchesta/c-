438c6c40102784714cd6a39f59b8cc43f2071216
polinchesta <88452595+polinchesta@users.noreply.github.com>
2022-05-24 13:25:25 +0300

    k

---
+++ Практика/practice10/job1/Program.cs
@@ -0,0 +1,52 @@
+﻿NoteBook note = new NoteBook("sdf", 10);
+General general = new General("sdf", 10, "oil");
+
+Console.WriteLine(note.Price());
+Console.WriteLine(general.Price());
+class NoteBook
+{
+    private string _name;
+    private int _k;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public int K
+    {
+        get { return _k; }
+        set { _k = value; }
+    }
+
+    public double Price()
+    {
+        return 15 * _k;
+    }
+
+    public NoteBook(string nameData, int kData)
+    {
+        _name = nameData;
+        _k = kData;
+    }
+}
+
+class General : NoteBook
+{
+    private string _material;
+    public string Material
+    {
+        get { return _material; }
+        set { _material = value; }
+    }
+    public General(string nameData, int kData, string material)
+    : base(nameData, kData)
+    {
+        Material = material;
+    }
+    public double Price()
+    {
+        return 15 * K + 50;
+    }
+}
---
+++ Практика/practice10/job2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace job2
+{
+    class A
+    {
+        protected int a;
+        protected int b;
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public A()
+        {
+            this.a = default(int);
+            this.b = default(int);
+        }
+    }
+
+    class B : A
+    {
+        private int d;
+        public B(int a, int b, int d)
+            : base(a, b)
+        {
+            this.d = d;
+        }
+        public B(int d)
+        {
+            this.d = d;
+        }
+        public int c2
+        {
+            get
+            {
+
+                var sum = 0;
+                while (a < 10)
+                {
+                    sum += b + d;
+                    a++;
+                }
+                return sum;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B(9, 3, 1); // (a, b, d)
+            Console.WriteLine(b.c2);
+            Console.ReadLine();
+        }
+    }
+}
