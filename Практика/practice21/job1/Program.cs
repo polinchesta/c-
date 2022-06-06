ebfec94fdd20745d4ecd8cefd4fc77e75345bcdd
polinchesta <88452595+polinchesta@users.noreply.github.com>
2022-06-06 14:10:22 +0300

    w

---
+++ Практика/practice21/job1/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Phone phone = new Phone();
+        phone.Launch("Android 5.1");
+        Console.WriteLine(phone.OS.Name);
+
+        phone.OS = OS.getInstance("Android 8");
+        Console.WriteLine(phone.OS.Name);
+
+        Console.ReadLine();
+    }
+}
+class Phone
+{
+    public OS OS { get; set; }
+    public void Launch(string osName)
+    {
+        OS = OS.getInstance(osName);
+    }
+}
+class OS
+{
+    private static OS instance;
+
+    public string Name { get; private set; }
+
+    protected OS(string name)
+    {
+        this.Name = name;
+    }
+
+    public static OS getInstance(string name)
+    {
+        if (instance == null)
+            instance = new OS(name);
+        return instance;
+    }
+}
