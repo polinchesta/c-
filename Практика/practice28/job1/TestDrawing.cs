b6f67f7fe9d4a5e71094d4e9b7beb71d4205c51b
polinchesta <88452595+polinchesta@users.noreply.github.com>
2022-06-14 13:27:46 +0300

    d

---
+++ Практика/practice28/job1/TestDrawing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace job1
+{
+    internal class TestDrawing:Control
+    {
+        protected override void OnRender (System.Windows.Media.DrawingContext drawingContext)
+        {
+            drawingContext.DrawLine(new Pen(Brushes.Red,2),
+                new Point (0,0),
+                new Point (this.Height, this.Width));
+
+            drawingContext.DrawLine(new Pen(Brushes.Red, 2),
+                new Point(0, this.Width),
+                new Point(this.Height, 0));
+
+            base.OnRender(drawingContext);
+        }
+            }
+
+}
