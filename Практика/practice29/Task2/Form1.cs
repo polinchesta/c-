586dc9e0aa3b95ff58ed5261de48d3cd4a53c9bc
polinchesta <88452595+polinchesta@users.noreply.github.com>
2022-06-16 13:11:36 +0300

    c

---
+++ Практика/practice29/Task2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap baner;
+        Rectangle rct;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                baner = new Bitmap("banner.jpg");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Ошибка загрузки файла баннера\n" + e.ToString(), "Баннер");
+                this.Close();
+                return;
+            }   
+
+            rct.X = 0;
+            rct.Y = 0;
+            rct.Width = baner.Width;
+            rct.Height = baner.Height;
+
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rct.X -= 1;
+            if (Math.Abs(rct.X) > rct.Width)
+                rct.X += rct.Width;
+            Invalidate();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Y < rct.Y + rct.Height) && (e.Y > rct.Y))
+            {
+                if (timer1.Enabled != false)
+                    timer1.Enabled = false;
+            }
+            else
+            {
+                if (timer1.Enabled != true)
+                    timer1.Enabled = true;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            for (int i = 0; i < Convert.ToInt16(this.ClientSize.Width / rct.Width) + 1; i++)
+                g.DrawImage(baner, rct.X + i * rct.Width, rct.Y);
+        }
+    }
+}
---
+++ Практика/practice29/Task3(additional)/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace z4_3
+{
+    public partial class Form1 : Form
+    {
+        const float p = (float)Math.PI;
+        Timer timer = new Timer() { Interval = 10 };
+        GraphicsPath sinusoid;
+        int n;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+            pictureBox1.Paint += pictureBox1_Paint;
+            timer.Tick += timer_Tick;
+            sinusoid = new GraphicsPath();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (++n >= sinusoid.PointCount) n = 0;
+            pictureBox1.Refresh();
+        }
+
+        void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (sinusoid.PointCount == 0) return;
+            PointF pt = sinusoid.PathPoints[n];
+            e.Graphics.ScaleTransform(1, -1);
+            e.Graphics.TranslateTransform(5, -pictureBox1.Height / 2);
+            e.Graphics.DrawPath(Pens.Black, sinusoid);
+            e.Graphics.FillEllipse(Brushes.Yellow, RectangleF.FromLTRB(pt.X - 15, pt.Y - 15, pt.X + 15, pt.Y + 15));
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            n = 0;
+            CreateSinusoid(sinusoid, 10f * p, pictureBox1.ClientSize);
+            if (timer.Enabled)
+                timer.Stop();
+            else
+                timer.Start();
+        }
+        private void CreateSinusoid(GraphicsPath GraphPath, float MaxX, Size size)
+        {
+            GraphPath.Reset();
+            PointF[] points = new PointF[1] { PointF.Empty };
+            for (float i = 0; i <= MaxX; i += 0.4f)
+            {
+                points[points.GetUpperBound(0)] = new PointF(i, (float)Math.Sin(i));
+                Array.Resize<PointF>(ref points, points.Length + 1);
+            }
+            Array.Resize<PointF>(ref points, points.Length - 1);
+            GraphPath.AddCurve(points);
+            Matrix m = new Matrix();
+            m.Scale((float)(size.Width / MaxX), 0.4f * size.Height);
+            GraphPath.Transform(m);
+        }
+    }
+}
